a4334e46c3bb2dce87f123e8be55575f1d546721
SL-CodeWorrier <chathuathnayaka@gmail.com>
2024-04-11 09:37:49 +0530

    Initial Commit

---
+++ ECommerceSportSwiftUi/ECommerceSportSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ECommerceSportSwiftUiApp.swift
+//  ECommerceSportSwiftUi
+//
+//  Created by Chathura Aththanayaka on 2024-04-11.
+//
+
+import SwiftUI
+
+@main
+struct ECommerceSportSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
