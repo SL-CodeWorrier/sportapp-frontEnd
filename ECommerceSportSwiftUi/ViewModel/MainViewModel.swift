94018aecb79e872fbd46d2f550462be59a86fc68
SL-CodeWorrier <chathuathnayaka@gmail.com>
2024-04-21 21:34:01 +0530

    line text field created

---
+++ ECommerceSportSwiftUi/ViewModel/MainViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewModel.swift
+//  ECommerceSportSwiftUi
+//
+//  Created by Chathura Aththanayaka on 2024-04-21.
+//
+
+import SwiftUI
+
+struct MainViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainViewModel()
+}
